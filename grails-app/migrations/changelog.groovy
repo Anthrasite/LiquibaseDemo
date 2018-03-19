a6992fff6c78534400070d983b03b2b5e7e7d3d9
Cole Kehler <kehlerc5@myumanitoba.ca>
2018-03-18 22:13:41 -0500

    Generate the initial changelog

---
+++ grails-app/migrations/changelog.groovy
@@ -0,0 +1,18 @@
+databaseChangeLog = {
+
+    changeSet(author: "cole (generated)", id: "1521426026638-1") {
+        createTable(tableName: "user") {
+            column(autoIncrement: "true", name: "id", type: "BIGINT") {
+                constraints(primaryKey: "true", primaryKeyName: "userPK")
+            }
+
+            column(name: "version", type: "BIGINT") {
+                constraints(nullable: "false")
+            }
+
+            column(name: "username", type: "VARCHAR(255)") {
+                constraints(nullable: "false")
+            }
+        }
+    }
+}
